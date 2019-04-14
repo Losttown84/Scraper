bed5f2d106b06dcc3579c72c1f36ba0e264c5279
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-14 10:25:48 -0500

    server.js file updated to include our necessities for set up to go live

---
+++ views/server.js
@@ -0,0 +1,22 @@
+// Dependencies
+var express = require("express");
+
+// Set up port
+var PORT = process.env.PORT || 3000;
+
+// Initiate express app
+var app = express();
+
+// express router set up
+var router = express.Router();
+
+// public folder for directory
+app.use(express.stats(__dirname + "/public"));
+
+// requests go here
+app.use(router);
+
+// i'm listening
+app.listen(PORT, function() {
+  console.log("Listening on port:" PORT);
+});
