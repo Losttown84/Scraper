28c5357f2305e6f4a87db356630873bf824552a9
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-14 10:50:44 -0500

    additional dependencies added to server.js file

--- views/server.js
+++ views/server.js
@@ -1,5 +1,7 @@
 // Dependencies
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 // Set up port
 var PORT = process.env.PORT || 3000;
@@ -12,6 +14,17 @@ var router = express.Router();
 
 // public folder for directory
 app.use(express.static(__dirname + "/public"));
+
+// connect handlebars to express
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// bodyParser for app
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 // requests go here
 app.use(router);
