81b6325df6ba682dc363f7d93d13371441b6f473
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-19 16:37:47 -0500

    route for fetching added

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,26 @@
+var scrape = require("../scripts/scraped");
+var headlineControl = require("../controllers/headlines");
+var noteControl = require("../controllers/notes");
+
 module.exports = function(router) {
   router.get("/", function(req, res) {
     res.render("home");
   });
   router.get("/saved", function(req, res) {
     res.render("saved");
+  });
+  router.get("/api/fetch", function(req, res) {
+    headlineControl.fetch(function(err, docs) {
+      if(!docs || docs.insertedCount === 0) {
+        res.json({
+          message: "Nothing new to see here. Check tomorrow"
+        });
+      }
+      else {
+        res.json({
+          message: "New items added: " + docs.insertedCount
+        });
+      }
+    });
   });
 }
