e08f49054d6c5483cfdf9f3b88ea4c01131b5ce0
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-19 14:29:42 -0500

    export information input to grab and store articles from mlb.com

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -1,8 +1,20 @@
-var scarped = require("../scripts/scraped");
-var makeDate = require("../scripts/date");
+var scarped = require("../script/scraped");
+var makeDate = require("../script/date");
 
 var Headline = require("../models/Headline");
 
 module.exports = {
-  
+  fetch: function(cb) {
+    scarped(function(data) {
+      var articles = data;
+      for (var i=0; i < articles.length; i++) {
+        articles[i].date = makeDate();
+        articles[i].saved = false;
+      }
+      Headline.collection.insertMany(articles, {ordered:flase}, function(err, docs) {
+        cb(err, docs);
+      });
+
+    });
+  }
 }
